987b962ed41afb6f57a96766c834ffae00d51e3d
iNet Digital <jcockburn@inetdigital.co.uk>
2025-05-11 13:59:13 +0100

    Update hubspot form overlay padding

--- src/components/ui/HubSpotFormOverlay.js
+++ src/components/ui/HubSpotFormOverlay.js
@@ -20,7 +20,7 @@ const HubSpotFormOverlay = ({ portalId }) => {
     <div className="fixed z-50">
       {isFormOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 overflow-y-scroll">
-          <div className="bg-white p-16 rounded-lg shadow-lg relative max-w-4xl w-full overflow-y-scroll mx-6">
+          <div className="bg-white p-0 sm:p-4 md:p-8 lg:p-16 rounded-lg shadow-lg relative max-w-4xl w-full overflow-y-scroll mx-6">
             <button
               onClick={closeForm}
               className="absolute top-2 right-2 text-gray-600 hover:text-black"
