5f5e3b8d2c8587d90574429784c235cb20951077
inetdigital <jcockburn@inetdigital.co.uk>
2026-08-25 11:17:49 +0100

    Adds development redirects

--- next.config.js
+++ next.config.js
@@ -12,6 +12,22 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/lacuna",
+        destination:
+          "/development/lacuna-gardens?utm_source=signage&utm_medium=shorturl&utm_campaign=lacuna-show-home",
+        permanent: false,
+      },
+      {
+        source: "/salo",
+        destination:
+          "/development/salo-bay-fistral-newquay-cornwall?utm_source=signage&utm_medium=shorturl&utm_campaign=salo-bay-launch",
+        permanent: false,
+      },
+    ];
+  },
   swcMinify: true, // Enable SWC minification for faster builds and smaller JS bundles
 };
 
